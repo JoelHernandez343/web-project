0c8f5cd66a238fa815ed473ebd4e7f1fc78ac444
marbrehi <54752485+marbrehi@users.noreply.github.com>
2019-11-10 12:49:28 -0600

    Add files via upload

---
+++ frontend/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.carousel').carousel();
+    $('.collapsible').collapsible();
+});
+$(".dropdown-trigger").dropdown();
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+});
