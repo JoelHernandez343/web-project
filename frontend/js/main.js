e9da6094447419d1aa064c69ed0d1c38807c5759
marbrehi <marbrehi@gmail.com>
2019-11-14 23:39:23 -0600

    Login, Sign Up, Account actualizados

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     $('.parallax').parallax();
     $('select').formSelect();
     $('.datepicker').datepicker();
+    $('.collapsible').collapsible();
 });
 
 $(function() {
