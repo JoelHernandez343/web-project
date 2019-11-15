dcc3a2242a221782eb95999b742e7efb166f98e6
marbrehi <marbrehi@gmail.com>
2019-11-14 20:45:46 -0600

    Vistas de categorías,, sing up casi listo y enviar (:smile:)

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,3 +12,13 @@ $(document).ready(function() {
     $('select').formSelect();
     $('.datepicker').datepicker();
 });
+
+$(function() {
+    $("#fecha").datepicker({
+        //maxDate: new Date(2019, 10, 13), //un mes antes del que se quiera xd
+        minDate: new Date(1900, 31, 1),
+        maxDate: new Date(2018, 11, 14),
+        defaultDate: new Date(2018, 11, 1),
+        yearRange: 10,
+    });
+});
