a1024dd263bfbfc6578112237220cde3acc9a38b
Elias-98 <sanchezem99@hotmail.com>
2019-11-20 16:47:33 -0600

    Al pedernal, conexion base, falta cambiar valores de respuesta en el account.html

--- backend/confiBD.php
+++ backend/confiBD.php
@@ -1,14 +1,15 @@
 <?php
     $servidorBD = "localhost";
 	$usuarioBD = "root";
-	$contrasenaBD = "root";
-	$nombreBD = "joel_2";
+	$contrasenaBD = "";
+	$nombreBD = "bd3"; 
 	$conexion = mysqli_connect($servidorBD,$usuarioBD,$contrasenaBD,$nombreBD);
 	mysqli_query($conexion, "SET NAMES 'utf8'"); //Esta instrucción permite guardar eñes y acentos en la BD ;)
 	if(mysqli_connect_errno($conexion)){
 		die("Problemas con la conexi&oacute;n al servidor MySQL: ".mysqli_connect_error());
 	}else{
-		mysqli_query($conexion, "SET NAMES 'utf8'"); //Esta instrucción permite guardar eñes y acentos en la BD ;)
+		mysqli_query($conexion, "SET NAMES 'utf8'"); //Esta instrucción permite guardar eñes y acentos en la BD ;
+		
 	}
 
 ?>
--- backend/newAcount_AX.php
+++ backend/newAcount_AX.php
@@ -1,1 +1,18 @@
-/* aqui un codigo perro */
+<?php
+    include("./configBD.php");
+    include("./getPosts.php");
+
+    $contrasena = ($contrasena);//BASE SIN MD5
+    $respAX = array();
+    $sqlbb3 = "SELECT * FROM persona WHERE correo = '$email' AND contrasena = '$contrasena'";
+    $sqlbb3 = mysqli_query($conexion,$sqlbb3);
+    $infbb3 = mysqli_fetch_row($resbb3);
+    if(mysqli_num_rows($resbb3) == 1){
+        $respAX["val"] = 1;
+        $respAX["msj"] = "<h5 class='center-align'>Bienvenido $infbb3[1] $infbb3[2] :)</h5>";
+    }else{
+        $respAX["val"] = 0;
+        $respAX["msj"] = "Error";
+    }
+    echo json_encode($respAX);
+?>
