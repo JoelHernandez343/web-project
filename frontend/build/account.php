01ae8373fca450db0da1456544ab7730757e4284
Joel Hernández <joel.programador@gmail.com>
2019-11-26 21:10:19 -0600

    Trabajando en el account.php

--- frontend/build/account.php
+++ frontend/build/account.php
@@ -35,92 +35,105 @@
         </div>
       </nav>
     </div>
-    <ul class="row center" id="bodying_user">
-      <div>
-        <div class="row">
-          <div class="col 20 s12 m6 l6"><img class="right" style="max-height: 200px;" src="<?php echo $foto;  ?>" class="responsive-img" alt="" id="profile"/></div>
-          <div class="s12 m6 l6">
-            <h3 class="left-align" id="name_profile">Hello</h3> 
-            <h2 class="left-align" id="name_profile">BENBENOU:<?php echo "$infPersona[4] $infPersona[5] $infPersona[6]"; ?> </h2>
+    
+    <div class="container">
+
+      <div class="row center" id="bodying_user">
+        <div>
+          <div class="row">
+            <div class="col 20 s12 m6 l4 center"><img style="max-height: 200px;" src="<?php echo $foto;  ?>" class="responsive-img" alt="" id="profile"/></div>
+            <div class="col s12 m6 l8">
+              <h3 class="left-align" id="name_profile">Bienvenido</h3> 
+              <h2 class="left-align" id="name_profile"><?php echo "$infPersona[4] $infPersona[5] $infPersona[6]"; ?> </h2>
+            </div>
+            
+            <div class="center col 20 s12">
+              <h4 class="left" id="titlerow">Mis datos</h4>
+              <div class="col center input-field s12"><i class="material-icons prefix">account_circle</i>
+                <input class="validate" disabled="disabled" id="icon_prefix" type="text"/>
+                <label for="icon_prefix"><?php echo "$infPersona[4] $infPersona[5] $infPersona[6]"; ?></label><span class="helper-text">Nombre</span><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit </a></i>
+              </div>
+              <div class="col center input-field col s12 m12 l6"><i class="material-icons prefix">perm_contact_calendar</i>
+                <input class="validate" disabled="disabled" id="fecha" type="text"/>
+                <label for="fecha"><?php echo $infPersona[2]; ?></label><span class="helper-text">Fecha de nacimiento</span><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit </a></i>
+              </div>
+              <div class="col center input-field col s12 m12 l6"><i class="material-icons prefix">email</i>
+                <input class="validate" id="icon_email" type="email"/>
+                <label for="icon_email"><?php echo $infPersona[1]; ?></label><span class="helper-text" data-error="Incompleto, corrige tus datos" data-success="Correcto">Email</span><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit</a></i>
+              </div>
+              <div class="col center input-field col s12 m12 l6">
+                <select disabled="disabled">
+                  <option value="" disabled="disabled" selected="selected"><?php echo $genero; ?></option>
+                  <option value="F">Femenino</option>
+                  <option value="M">Masculino</option>
+                </select>
+                <label for="icon_gen"><i class="tiny material-icons">perm_identity</i>
+                  <label>Género</label>
+                </label><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit</a></i>
+              </div>
+            </div>
+
           </div>
         </div>
-      </div>
-      <div>
+      
+
         <div class="row">
-          <div class="col 20 s12 m4 l4">
-            <h4 class="left" id="titlerow">Mis datos</h4>
+          <div class="left col s12 m6 l6">
+            <ul class="collapsible">
+              <li>
+                <div class="collapsible-header"><i class="material-icons">card_membership </i><a class="black-text">Enviadas</a><span class="new badge amber">4</span></div>
+                <div class="collapsible-body">
+                  <ul class="row center" id="titlerow">
+                    <div class="center col s6 m8 l8">Historial</div>
+                    <div class="center col s6 m4 l4">Fecha</div>
+                  </ul>
+                  <ul class="row center">
+                    <div class="center col s6 m8 l8">Hola, mai friend<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
+                    <div class="center col s6 m4 l4">18/03/19</div>
+                  </ul>
+                  <ul class="row center">
+                    <div class="center col s6 m8 l8">Querida Trixie<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
+                    <div class="center col s6 m4 l4">01/12/18</div>
+                  </ul>
+                </div>
+              </li>
+            </ul>
           </div>
-          <div class="center col 20 s12 m8 l8">
-            <div class="center input-field s12"><i class="material-icons prefix">account_circle</i>
-              <input class="validate" disabled="disabled" id="icon_prefix" type="text"/>
-              <label for="icon_prefix"><?php echo "$infPersona[4] $infPersona[5] $infPersona[6]"; ?></label><span class="helper-text">Nombre</span><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit </a></i>
-            </div>
-            <div class="center input-field col s12 m12 l6"><i class="material-icons prefix">perm_contact_calendar</i>
-              <input class="validate" disabled="disabled" id="fecha" type="text"/>
-              <label for="fecha"><?php echo $infPersona[2]; ?></label><span class="helper-text">Fecha de nacimiento</span><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit </a></i>
-            </div>
-            <div class="center input-field col s12 m12 l6"><i class="material-icons prefix">email</i>
-              <input class="validate" id="icon_email" type="email"/>
-              <label for="icon_email"><?php echo $infPersona[1]; ?></label><span class="helper-text" data-error="Incompleto, corrige tus datos" data-success="Correcto">Email</span><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit</a></i>
-            </div>
-            <div class="center input-field col s12 m12 l6">
-              <select disabled="disabled">
-                <option value="" disabled="disabled" selected="selected"><?php echo $genero; ?></option>
-                <option value="F">Femenino</option>
-                <option value="M">Masculino</option>
-              </select>
-              <label for="icon_gen"><i class="tiny material-icons">perm_identity</i>
-                <label>Género</label>
-              </label><i class="right tiny material-icons"><a class="grey-text" href="./index.html">&nbspmode_edit</a></i>
-            </div>
+          <div class="col s12 m6 l6">
+            <ul class="collapsible">
+              <li>
+                <div class="collapsible-header"><i class="material-icons">card_giftcard </i><a class="black-text">Recibidas</a><span class="new badge green">4</span></div>
+                <div class="collapsible-body">
+                  <ul class="row center" id="titlerow">
+                    <div class="center col s6 m8 l8">Historial</div>
+                    <div class="center col s6 m4 l4">Fecha</div>
+                  </ul>
+                  <ul class="row center">
+                    <div class="center col s6 m8 l8">Hola, soy yo de nuevo<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
+                    <div class="center col s6 m4 l4">18/03/19</div>
+                  </ul>
+                  <ul class="row center">
+                    <div class="center col s6 m8 l8">Que ondii<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
+                    <div class="center col s6 m4 l4">01/12/18</div>
+                  </ul>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
+
+
+
+        
       </div>
-    </ul>
-    <ul class="row center" id="bodying">
-      <div class="left col s12 m6 l6">
-        <ul class="collapsible">
-          <li>
-            <div class="collapsible-header"><i class="material-icons">card_membership </i><a class="black-text">Enviadas</a><span class="new badge amber">4</span></div>
-            <div class="collapsible-body">
-              <ul class="row center" id="titlerow">
-                <div class="center col s6 m8 l8">Historial</div>
-                <div class="center col s6 m4 l4">Fecha</div>
-              </ul>
-              <ul class="row center">
-                <div class="center col s6 m8 l8">Hola, mai friend<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
-                <div class="center col s6 m4 l4">18/03/19</div>
-              </ul>
-              <ul class="row center">
-                <div class="center col s6 m8 l8">Querida Trixie<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
-                <div class="center col s6 m4 l4">01/12/18</div>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div class="left col s12 m6 l6">
-        <ul class="collapsible">
-          <li>
-            <div class="collapsible-header"><i class="material-icons">card_giftcard </i><a class="black-text">Recibidas</a><span class="new badge green">4</span></div>
-            <div class="collapsible-body">
-              <ul class="row center" id="titlerow">
-                <div class="center col s6 m8 l8">Historial</div>
-                <div class="center col s6 m4 l4">Fecha</div>
-              </ul>
-              <ul class="row center">
-                <div class="center col s6 m8 l8">Hola, soy yo de nuevo<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
-                <div class="center col s6 m4 l4">18/03/19</div>
-              </ul>
-              <ul class="row center">
-                <div class="center col s6 m8 l8">Que ondii<i class="tiny material-icons"><a class="grey-text" href="index.html/hola">&nbspnear_me</a></i></div>
-                <div class="center col s6 m4 l4">01/12/18</div>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </ul>
+
+
+
+
+    </div>
+
+
+
   </body>
 
   <footer class="page-footer fixed-container grey darken-4 s12 m12 l12">
